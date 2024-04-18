7e07d3e7e63b233341d604a7aee4bfc3d4057794
aasif <asifali8121@gmail.com>
2024-04-18 19:36:00 +0500

    basic js

---
+++ index.js
@@ -0,0 +1,24 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert('this is an alert!');
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'I like pizza!';
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "Bro";
+console.log(typeof firstName);
+console.log(firstName);
+
+let favouriteFood = "pizza"
+console.log(`You like ${favouriteFood}`)
+
+console.log(typeof gpa);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
