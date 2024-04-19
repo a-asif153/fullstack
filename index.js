acca3b1748bf6a5ffd65c0584fe898de61aafe10
aasif <asifali8121@gmail.com>
2024-04-19 16:36:31 +0500

    checkboxes

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+const myCheckBox = document.getElementById(`myCheckBox`);
+const visaBtn = document.getElementById(`visaBtn`);
+const masterCardBtn = document.getElementById(`masterCardBtn`);
+const payPalBtn = document.getElementById(`payPalBtn`);
+const mySubmit = document.getElementById(`mySubmit`);
+const subResult = document.getElementById(`subResult`);
+const paymentResult = document.getElementById(`paymentResult`);
 
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+     subResult.textContent = `You are subscribed!`;   
+    }
+    else{
+        subResult.textContent = `You are NOT subscribed!`; 
+    }
 
-const decreaseBtn = document.getElementById(`decreaseBtn`);
-const resetBtn = document.getElementById(`resetBtn`);
-const increaseBtn = document.getElementById(`increaseBtn`);
-const countLabel = document.getElementById(`countLabel`);
-let count = 0;
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with visa`;
+    }
 
-increaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
-}
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with master card`;
+    }
 
-decreaseBtn.onclick = function() {
-    count--;
-    countLabel.textContent = count;
-}
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with paypal`;
+    }
 
-resetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
+    else{
+        paymentResult.textContent = `You must select a payment type`;
+    }
 }
