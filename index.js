f412e988c0ef7e56ea88fffe6309eea26fb8a07a
aasif <asifali8121@gmail.com>
2024-04-19 15:43:08 +0500

    counter app

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
-const pi = 3.14159;
-let circumference;
 
-document.getElementById('mysubmit').onclick = function(){
-    radius = document.getElementById('userInput').value;
-    radius = Number(radius)
-    circumference = 2*pi*radius;
-    document.getElementById('answer').textContent = `The circumference of the circle is: ${circumference}`
+const decreaseBtn = document.getElementById(`decreaseBtn`);
+const resetBtn = document.getElementById(`resetBtn`);
+const increaseBtn = document.getElementById(`increaseBtn`);
+const countLabel = document.getElementById(`countLabel`);
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
 }
